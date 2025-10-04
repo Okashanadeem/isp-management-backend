f17de639625f79428a0785bf3267a0e7bb259b27
Okashanadeem <okashanadeem0101@gmail.com>
2025-10-04 14:55:59 +0500

    Finalize authentication module: JWT-based login/logout with secure password hashing

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 const router = Router();
-import { login, refresh, logout } from '../controllers/authController.js';
+import { login, logout } from '../controllers/authController.js';
 import validate from '../middlewares/validateMiddleware.js';
 import { loginValidator } from '../validators/authValidator.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 
 router.post('/login', validate(loginValidator), login);
-router.post('/refresh', refresh);        
 router.post('/logout', protect, logout);  
 
 export default router;
